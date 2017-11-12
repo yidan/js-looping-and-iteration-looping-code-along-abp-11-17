25a9f808a8e2ebb94d3ea368dc6ef230a311d5f9
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 16:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(emps){
   for (let i=0; i<emps.length;i++){
-    console.log(`Welcome ${emps[i]}! You are employee #{i+1}.`)
+    console.log(`Welcome ${emps[i]}! You are employee #${i+1}.`)
   }
   return emps;
 }
